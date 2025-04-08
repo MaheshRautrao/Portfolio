ef04a05f1882d802ec7c7fe8f5293b738d400429
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2025-04-08 07:16:19 +0530

    changes done to reusme

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -7,10 +7,7 @@ import { PdfRenderer } from "@/components/resume/pdf-renderer";
 export default function Resume() {
   return (
     <div className="flex flex-col items-center gap-4 py-10">
-      <Link
-        href="C:\Users\91801\Desktop\Portfolio-new\client\public\resume.pdf"
-        target="_blank"
-      >
+      <Link href="/resume.pdf" target="_blank">
         <Button>Download Resume</Button>
       </Link>
       <div className="shadow-lg">
